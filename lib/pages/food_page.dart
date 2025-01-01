6bbf188dfa00d6c3f97de7238b28db46e50af8ff
dino330340 <h.alaaeldin330@gmail.com>
2025-01-01 20:21:25 +0400

    ....

--- lib/pages/food_page.dart
+++ lib/pages/food_page.dart
@@ -1,11 +1,18 @@
+import 'package:firebase_food_delivery/components/my_button.dart';
 import 'package:flutter/material.dart';
 
 import '../model/food.dart';
 
 class FoodPage extends StatefulWidget {
   final Food food;
+  final Map<Addon, bool> selectedAddons = {};
 
-  const FoodPage({super.key, required this.food});
+  FoodPage({super.key, required this.food}) {
+    // initialize selected addons to be false
+    for (Addon addon in food.avaliableAddon) {
+      selectedAddons[addon] = false;
+    }
+  }
 
   @override
   State<FoodPage> createState() => _FoodPageState();
@@ -14,25 +21,107 @@ class FoodPage extends StatefulWidget {
 class _FoodPageState extends State<FoodPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Image.asset(widget.food.imagePath),
-          Text(widget.food.name),
-          Text(widget.food.description),
+    return Stack(
+      children: [
+        // scaffold UI
+        Scaffold(
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Image.asset(widget.food.imagePath),
+                Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.food.name,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      Text(
+                        '\$${widget.food.price}',
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: Theme.of(context).colorScheme.primary),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(widget.food.description),
+                      const SizedBox(height: 10),
+                      Divider(
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+                      const SizedBox(height: 10),
+                      // addons
+                      Text(
+                        "Add-ons",
+                        style: TextStyle(
+                            color: Theme.of(context).colorScheme.inversePrimary,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 10),
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Theme.of(context).colorScheme.secondary),
+                            borderRadius: BorderRadius.circular(8)),
+                        child: ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: widget.food.avaliableAddon.length,
+                            physics: const NeverScrollableScrollPhysics(),
+                            padding: EdgeInsets.zero,
+                            itemBuilder: (context, index) {
+                              // get individual addon
+                              Addon addon = widget.food.avaliableAddon[index];
+                              return CheckboxListTile(
+                                  title: Text(addon.name),
+                                  subtitle: Text(
+                                    '\$${addon.price}',
+                                    style: TextStyle(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .primary),
+                                  ),
+                                  value: widget.selectedAddons[addon],
+                                  onChanged: (bool? value) {
+                                    setState(() {
+                                      widget.selectedAddons[addon] = value!;
+                                    });
+                                  });
+                            }),
+                      ),
+                    ],
+                  ),
+                ),
+                // Add to cart button
+                MyButton(onTap: () {}, text: "Add to cart"),
+                const SizedBox(height: 25),
+              ],
+            ),
+          ),
+        ),
 
-          // addons
-          ListView.builder(
-            shrinkWrap: true,
-              itemCount: widget.food.avaliableAddon.length,
-              physics: const NeverScrollableScrollPhysics(),
-              itemBuilder: (context, index) {
-            return CheckboxListTile(value: false, onChanged: (value){}
-            );
-          }
-          )
-        ],
-      ),
+        // back button
+        SafeArea(
+          child: Opacity(
+            opacity: 0.5,
+            child: Container(
+              margin: const EdgeInsets.only(left: 25),
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.secondary,
+                shape: BoxShape.circle
+              ),
+              child: IconButton(
+                icon: const Icon(Icons.arrow_back_rounded),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
